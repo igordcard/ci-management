1b6c1a6a1e4ffa9eaf8580e21f171021a334a272
Andrew Grimberg <agrimberg@linuxfoundation.org>
2018-06-25 14:38:29 -0700

    Add global vars management
    Start managing the global vars via Jenkins
    
    Change-Id: I39a8056d24d32d1321cc5becee87c1e25386eee1
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ jenkins-config/global-vars-production.sh
@@ -0,0 +1,7 @@
+GERRIT_URL=https://gerrit.akraino.org/r
+GIT_URL=ssh://akraino-jobbuilder@gerrit.akraino.org:29418
+JENKINS_HOSTNAME=vex-yul-akraino-jenkins-prod-1
+LOGS_SERVER=https://logs.akraino.org
+NEXUS_URL=https://nexus.akraino.org
+SILO=production
+SONAR_URL=https://sonar.akraino.org
