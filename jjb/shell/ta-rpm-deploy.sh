43090d9d26878bcabc7d46c0cce42f96ff7aaf6a
Saku Chydenius <saku.chydenius@nokia.com>
2019-05-15 16:54:42 +0300

    [ta] Add TA verify and merge jobs
    Includes:
    
        - TA-specific macros and templates to akraino_templates/
    
        - Verify and merge jobs for all the "ta/*" git repositories that
          produce RPMs. Merge jobs will upload RPMs to Nexus repository
          "images-snapshots".
    
        - Verify jobs for the three supporting tool and configuration git
          repositories ta/{manifest|build-tools|rpmbuilder}
    
        - Image build job to produce the TA end product: ISO image
    
        - Publish ISO images to Nexus
    
        - Moved shell scripts to jjb/shell
    
    Signed-off-by: Saku Chydenius <saku.chydenius@nokia.com>
    Change-Id: I2b1ecd6f34c2dd5f5c8fbb9ff09ab180d943a3d9

---
+++ jjb/shell/ta-rpm-deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+echo "---> ta-rpm-deploy.sh"
+
+# Ensure we fail the job if any steps fail.
+set -eu -o pipefail
+
+set -x  # Trace commands for this script to make debugging easier.
+
+NEXUS_REPO=images-snapshots
+release_path=TA/release-1
+
+repo_dir="$WORKSPACE/work/nexus/$NEXUS_REPO"
+x86_dir="$repo_dir/$release_path/rpms/x86_64"
+sources_dir="$repo_dir/$release_path/rpms/Sources"
+nexus_repo_url="$NEXUS_URL/content/repositories/$NEXUS_REPO"
+
+mkdir -p "$x86_dir"
+mkdir -p "$sources_dir"
+
+cp "$WORKSPACE/work/results/repo/"*.rpm "$x86_dir"
+cp "$WORKSPACE/work/results/src_repo/"*.rpm "$sources_dir"
+
+echo "-----> Upload RPMs to Nexus"
+lftools deploy nexus "$nexus_repo_url" "$repo_dir"
+
+set +x  # Disable trace since we no longer need it.
+echo "RPMs location: <a href=\"$nexus_repo_url\">$nexus_repo_url</a>"
