8a8805699c7322bf5f795dc7626beba096efc296
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2019-05-22 10:09:31 +0800

    Add 3 virtual hosts into IEC CI/CD platform
    In this patch, the 3 virtual host was deployed on CI platform.
    Firstly, it add the 3rd host into k8-nosdn-nofeature-noha.yml
    confiugration file.
    Secondly, it modifies the confiugration file for avoiding install
    K8s by compass.
    
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>
    Change-Id: I5b1b6d4140a37182137325668b4bc7076c8b4f1d

--- jjb/iec/iec-compass-deploy.sh
+++ jjb/iec/iec-compass-deploy.sh
@@ -3,5 +3,21 @@ set -e
 
 cd compass4nfv
 
+# Create 3 virtual machine
+echo -e "  - name: host3\n    roles:\n      - kube_node" >> deploy/conf/vm_environment/k8-nosdn-nofeature-noha.yml
+# Remove useless code
+sed -i "33,90d" deploy/adapters/ansible/kubernetes/ansible-kubernetes.yml
+
+export ADAPTER_OS_PATTERN='(?i)ubuntu-16.04.*arm.*'
+export OS_VERSION="xenial"
+export KUBERNETES_VERSION="v1.13.0"
+if [[ "$NODE_NAME" =~ "-virtual" ]]; then
+    export DHA="deploy/conf/vm_environment/k8-nosdn-nofeature-noha.yml"
+    export NETWORK="deploy/conf/vm_environment/network.yml"
+    export VIRT_NUMBER=3 VIRT_CPUS=2 VIRT_MEM=4096 VIRT_DISK=50G
+fi
+
+./deploy.sh
+
 echo "Compass Deploy successful"
 exit 0
