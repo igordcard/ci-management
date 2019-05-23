2d02ba2262233b3dc806b6d95551713407d1faae
trevor tao <trevor.tao@arm.com>
2019-05-23 16:48:10 +0800

    Add IEC Foundation Framework Deployment Work
    Add IEC foundation deployment working scripts for
    IEC working environment and related verification to
    the environment.
    
    Signed-off-by: trevor tao <trevor.tao@arm.com>
    Change-Id: I40b255cd9412b75deb052acd01932fddec06a826
    Signed-off-by: trevor tao <trevor.tao@arm.com>

--- jjb/iec/iec-compass-destroy.sh
+++ jjb/iec/iec-compass-destroy.sh
@@ -2,4 +2,8 @@
 set -e
 
 rm -rf compass4nfv
+sudo virsh destroy host1
+sudo virsh destroy host2
+sudo virsh destroy host3
+
 exit 0
---
+++ jjb/iec/iec-fdn-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+sleep 10
+echo "Start IEC installation:"
+rm -rf iec
+rm -rf ~/.ssh/known_hosts
+
+git clone "https://gerrit.akraino.org/r/iec"
+
+cd iec/src/foundation/scripts
+
+sed -i.bak 's/10.169.36.152/10.1.0.50/g' ./config
+sed -i "/^K8S_MASTERPW=/cK8S_MASTERPW=\"root\"" ./config
+sed -i "/^HOST_USER=/cHOST_USER=\${HOST_USER:-root}" ./config
+sed -i "s/10.169.40.106,123456/10.1.0.51,root\"\n\"10.1.0.52,root/g" ./config
+HOST_USER=root
+export HOST_USER
+sudo ./startup.sh
+
+
+exit 0
