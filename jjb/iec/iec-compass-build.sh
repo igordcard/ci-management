67927999de6f8c75dc21059b3935279913855931
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2019-05-27 20:31:16 +0800

    Replace the root command with user mode
    The "sudo pip install" command cause some error when deploying
    compass. So replace the "sudo pip install" with the "python -m
     pip install pyyaml" command.
    
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>
    Change-Id: Ia86e5a8b1c75e6367d044e94e356f7d1a855418e

--- jjb/iec/iec-compass-build.sh
+++ jjb/iec/iec-compass-build.sh
@@ -7,12 +7,15 @@ git clone https://github.com/opnfv/compass4nfv.git
 cd compass4nfv
 
 COMPASS_WORK_DIR=$WORKSPACE/../compass-work
+
 mkdir -p $COMPASS_WORK_DIR
 ln -s $COMPASS_WORK_DIR work
 
 sudo docker rm -f `docker ps | grep compass | cut -f1 -d' '` || true
 
 curl -s http://people.linaro.org/~yibo.cai/compass/compass4nfv-arm64-fixup.sh | bash || true
+
+sed -i "s/sudo pip install pyyaml/python -m pip install pyyaml --user/g" build.sh
 
 ./build.sh
 
